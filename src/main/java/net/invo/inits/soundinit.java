4a4f83986867d60279c25a8c3a89b73e6b5f7caf
Globox1997 <mitschke11@web.de>
2020-04-29 00:53:34 -0500

    recipe change + activate totem

--- src/main/java/net/invo/inits/soundinit.java
+++ src/main/java/net/invo/inits/soundinit.java
@@ -12,12 +12,18 @@ public class soundinit {
     public static SoundEvent SLEEPGOLEMEVENT = new SoundEvent(SLEEPGOLEM);
     public static final Identifier TRADERSOUND = new Identifier("invo:tradersound");
     public static SoundEvent TRADERSOUNDEVENT = new SoundEvent(TRADERSOUND);
+    public static final Identifier TOTEMACTIVATE = new Identifier("invo:totemactivating");
+    public static SoundEvent TOTEMACTIVATEEVENT = new SoundEvent(TOTEMACTIVATE);
+    public static final Identifier TOTEMSLEEP = new Identifier("invo:totemsleep");
+    public static SoundEvent TOTEMSLEEPEVENT = new SoundEvent(TOTEMSLEEP);
 
     public static void init() {
 
         Registry.register(Registry.SOUND_EVENT, SLEEP, SLEEPEVENT);
         Registry.register(Registry.SOUND_EVENT, SLEEPGOLEM, SLEEPGOLEMEVENT);
         Registry.register(Registry.SOUND_EVENT, TRADERSOUND, TRADERSOUNDEVENT);
+        Registry.register(Registry.SOUND_EVENT, TOTEMACTIVATE, TOTEMACTIVATEEVENT);
+        Registry.register(Registry.SOUND_EVENT, TOTEMSLEEP, TOTEMSLEEPEVENT);
     }
 
 }
